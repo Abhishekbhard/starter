927580a2dc0ee37074a0c2dee30031de27500c4e
Abhishek <eduandabhi@gmail.com>
2023-03-25 14:31:49 +0530

    Increase Decrese

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,12 +14,24 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
-    remvoeItem: (state, action) => {
-      console.log(action);
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => {
+        return item.id !== itemId;
+      });
+    },
+    increase: (state, { payload }) => {
+      console.log(payload);
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
     },
   },
 });
 // console.log(cartSlice);
-export const { clearCart, remvoeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
